ec8a7f727e5616f6840686b237d4982ec00c2068
Chris Ozgo <chrisozgo99@gmail.com>
2023-11-22 23:29:33 +0100

    beginning of pdf scraping

--- scraping/main.js
+++ scraping/main.js
@@ -1,31 +1,8 @@
 import puppeteer from "puppeteer";
+import * as pdfjs from 'pdfjs-dist';
+
 import fs from "fs";
 import { computerEngineeringCourseRequirementsURLs } from "../utils/urls.js"
-
-function scrape() {
-    return new Promise(async (resolve, reject) => {
-        const urlPrefix = "gatech.edu"
-
-        try {
-            const browser = await puppeteer.launch({
-                headless: false,
-            });
-            const page = await browser.newPage();
-            await page.goto("https://degreeworks.gatech.edu/files/concentration_mgt.pdf");
-            const html = await page.content();
-            await browser.close();
-            // save html to a file
-            fs.writeFile("data/test.html", html, function(err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            resolve(html);
-        } catch (e) {
-            reject(e);
-        }
-    });
-}
 
 async function scrapeSite(url, headless = true) {
     const browser = await puppeteer.launch({
@@ -95,6 +72,24 @@ async function courseRequirementsToJson(page, browser) {
     return json;
 }
 
+async function fourYearPlanToJson(url) {
+    let finalString = '';
+    pdfjs.getDocument(url).promise.then(function(pdf) {
+        pdf.getPage(1).then(function(page) {
+            page.getTextContent().then(function(textContent) {
+                const textItems = textContent.items;
+                finalString = textItems.map(function(item) {
+                    return item.str;
+                }).join(' ');
+                console.log(finalString);
+            });
+        });
+    });
+
+    return finalString;
+
+}
+
 async function processHtml(page) {
     // return text from the webpage
     const text = await page.$eval('*', (el) => el.innerText);
@@ -103,13 +98,16 @@ async function processHtml(page) {
 }
 
 async function main() {
-    for (let i = 0; i < computerEngineeringCourseRequirementsURLs.length; i++) {
-        const url = computerEngineeringCourseRequirementsURLs[i];
-        if (url) {
-            const {page, browser} = await scrapeSite(url, false);
-            await courseRequirementsToJson(page, browser);
-        }
-    }
+    // for (let i = 0; i < computerEngineeringCourseRequirementsURLs.length; i++) {
+    //     const url = computerEngineeringCourseRequirementsURLs[i];
+    //     if (url) {
+    //         const {page, browser} = await scrapeSite(url, false);
+    //         await courseRequirementsToJson(page, browser);
+    //     }
+    // }
+
+    const pdfUrl = 'https://ece.gatech.edu/sites/default/files/documents/undergraduate/curriculum-threads/ee/bioees.pdf';
+    fourYearPlanToJson(pdfUrl);
 }
 
 main();
